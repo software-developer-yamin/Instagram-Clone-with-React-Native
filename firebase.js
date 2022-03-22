8205dad1fa49b40765233105f8290d6712de77da
DeveloperYamin <developer.yamin@gmail.com>
2022-03-22 14:59:27 +0600

    setUp firebase

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+     apiKey: "AIzaSyDlEnC5iYsovAKFoppZ8O_8DfdC04laIfY",
+     authDomain: "instagramclone-react-nat-b027d.firebaseapp.com",
+     projectId: "instagramclone-react-nat-b027d",
+     storageBucket: "instagramclone-react-nat-b027d.appspot.com",
+     messagingSenderId: "626591948962",
+     appId: "1:626591948962:web:e990cd0597f4a4ebbd37fb",
+     measurementId: "G-8PCQNGTMCY"
+};
+
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+
+export { db, auth };
